1efa5802e5819ef24ba63b2a500fd7cc1f7aef43
Antrei Ntenisov <36208861+andrewjmathers@users.noreply.github.com>
2019-01-13 22:27:22 +0100

    Add files via upload

---
+++ precache-manifest.10ea99e57b6dda60f5e9bc58e3d2e8aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5af5f8bdf258bfd74a",
+    "url": "/react-interview-project/static/js/runtime~main.5d5af5f8.js"
+  },
+  {
+    "revision": "1c5654196f6d18f6bb40",
+    "url": "/react-interview-project/static/js/main.1c565419.chunk.js"
+  },
+  {
+    "revision": "d70a305e4e51204c7a85",
+    "url": "/react-interview-project/static/js/1.d70a305e.chunk.js"
+  },
+  {
+    "revision": "1c5654196f6d18f6bb40",
+    "url": "/react-interview-project/static/css/main.7e24d8cd.chunk.css"
+  },
+  {
+    "revision": "8abb4f36a40226c7bad2742755d5525a",
+    "url": "/react-interview-project/index.html"
+  }
+];
