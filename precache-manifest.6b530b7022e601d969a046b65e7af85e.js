14f7d2115ded7565ac3f046132f2ce65dcc05ec6
Andrey Denisov <AndrewMathers@Andreys-MacBook-Air.local>
2019-01-13 21:40:45 +0100

    Updates

---
+++ precache-manifest.6b530b7022e601d969a046b65e7af85e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5af5f8bdf258bfd74a",
+    "url": "/react-interview-project/static/js/runtime~main.5d5af5f8.js"
+  },
+  {
+    "revision": "1d8a2796914dc63345a5",
+    "url": "/react-interview-project/static/js/main.1d8a2796.chunk.js"
+  },
+  {
+    "revision": "d70a305e4e51204c7a85",
+    "url": "/react-interview-project/static/js/1.d70a305e.chunk.js"
+  },
+  {
+    "revision": "1d8a2796914dc63345a5",
+    "url": "/react-interview-project/static/css/main.7e24d8cd.chunk.css"
+  },
+  {
+    "revision": "f1a4bf04852263cc482ee39187f615a0",
+    "url": "/react-interview-project/index.html"
+  }
+];
